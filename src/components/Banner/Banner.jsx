05d49b4f5c2cf3cb9065e5f9e5a1e1aeb3709b1f
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-23 23:54:37 +0800

    Add Banner component
    Introduces a new Banner React component with associated CSS and an index file for exports. Includes a placeholder README for documentation.

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import classNames from "classnames"
+import "./Banner.css"
+import "../../index.css"
+import React from "react"
+
+export default function Banner() {
+    return <h1>hi im a banner</h1>
+}
+
