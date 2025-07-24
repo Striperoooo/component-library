e2f1554f7284b59521b4d3b21a426f89d93608ef
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-24 22:10:37 +0800

    Add Banner component with styles and icon support
    Introduces a new Banner component with customizable type, title, and description, using react-icons for status icons. Includes corresponding CSS for layout and theming, updates App.jsx to render the Banner, and adds react-icons as a dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react"
 import "./index.css"
 import Badge from "./components/Badge"
+import Banner from "./components/Banner"
 
 
 function App() {
 
   return (
     <>
-      <Badge badgeColor="green" shape="pill" className="fart">""</Badge>
+
+      <Banner type=""
+        title="I am the title"
+        className=""
+      >
+      </Banner>
     </>
   )
 }
+
+// <Badge badgeColor="green" shape="pill" className="fart">""</Badge>
 
 export default App
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,23 @@
 import classNames from "classnames"
+import { FaCheckCircle, FaInfoCircle } from "react-icons/fa";
+import { IoIosWarning, IoIosCloseCircle } from "react-icons/io";
 import "./Banner.css"
 import "../../index.css"
 import React from "react"
 
-export default function Banner() {
-    return <h1>hi im a banner</h1>
+export default function Banner({ type, title, className, children }) {
+    const badgeTypeClassName = type || "success"
+    const bannerClassName = classNames("banner", badgeTypeClassName, className)
+    const role = type === "error" || type === "warning" ? "alert" : "status"
+
+    const bannerTitle = title || "Banner Title"
+
+    return (
+        <div className={bannerClassName} role={role}>
+            <FaCheckCircle className="icon" />
+            <div className="banner-title">{bannerTitle}</div>
+            {children && <div className="banner-desc">{children}</div>}
+        </div>
+    )
 }
 
