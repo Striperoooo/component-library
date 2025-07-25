6b9f156eaf19009c7d5fbe233a913b5dd1dd6219
Charles Deiniel D. Villanueva <charlesdvillanueva16@gmail.com>
2025-07-25 19:54:11 +0800

    Add Card component UI and styles
    Implemented the Card component with a cloud upload icon, title, and content. Added corresponding CSS for layout, styling, and hover effects.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,18 @@
 import classNames from "classnames"
+import { IoCloudUploadOutline } from "react-icons/io5";
 import "./Card.css"
 import "../../index.css"
 import React from "react"
 
 export default function Card() {
-    return
+    return (
+        <div className="card-container">
+            <div className="icon">
+                <IoCloudUploadOutline />
+            </div>
+            <div className="title">Easy Deployment</div>
+            <div className="content">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</div>
+        </div>
+    )
 }
 
